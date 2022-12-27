004e8a334639a16c205f8d3c486baaf26530f0c6
Abhishek Salmote <99242107+abhisalmote@users.noreply.github.com>
2022-12-27 13:45:08 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,36 @@
+let progress = document.getElementById("progress_bar");
+let song = document.getElementById("song");
+let ctrlIcon = document.getElementById("ctrlIcon");
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+function playPause(){
+    if(ctrlIcon.classList.contains("fa-pause")){
+        song.pause();
+        ctrlIcon.classList.add("fa-play");
+        ctrlIcon.classList.remove("fa-pause");
+    }else{
+        song.play();
+        ctrlIcon.classList.add("fa-pause");
+        ctrlIcon.classList.remove("fa-play");
+    }
+}
+
+if(song.play()){
+    setInterval(() =>{
+        progress.value = song.currentTime;
+    },500);
+}
+
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value;
+    ctrlIcon.classList.add("fa-pause");
+    ctrlIcon.classList.remove("fa-play");
+}
+
+
+
